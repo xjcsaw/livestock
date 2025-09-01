946b7453d766bf24d1f45b51cea1d6ec61819231
Dennis-Oliver Steiner <dsteiner@brockhaus-ag.de>
2025-09-01 20:44:27 +0200

    initial commit

---
+++ backend/src/test/java/bag/livestock/livestock/LivestockApplicationTests.java
@@ -0,0 +1,13 @@
+package bag.livestock.livestock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivestockApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    BrowserModule,
+    AppComponent
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
